9740a15e8497f612c9ca4a55732d5c88b5921f58
Hunter Wignall <hwignall2000@gmail.com>
2019-05-12 09:59:51 -0400

    Update test

--- src/test/java/EncryptionTest.java
+++ src/test/java/EncryptionTest.java
@@ -1,15 +1,9 @@
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
@@ -17,66 +11,91 @@ import org.junit.jupiter.api.Test;
 import simplenet.Client;
 import simplenet.Server;
 import simplenet.packet.Packet;
+import simplenet.utility.exposed.CryptoFunction;
 
 public class EncryptionTest {
-    private final String RSA = "RSA/ECB/PKCS1Padding", AES = "AES/CBC/PKCS5Padding";
-    private final Server server = new Server();
     private final ThreadLocalRandom random = ThreadLocalRandom.current();
     private final byte[] sharedSecret = new byte[16];
-    private KeyPairGenerator keyPairGenerator;
-    private Cipher rsaClient, rsaServer;
-    private Cipher serverEncryption, serverDecryption;
-    private Cipher clientEncryption, clientDecryption;
+    
     private final byte[] encryptBytes = new byte[128];
     
     @Test
-    void init() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException, InterruptedException {
-        keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-        keyPairGenerator.initialize(1024);
+    void init() {
         random.nextBytes(sharedSecret);
-        
-        final KeyPair rsaKeys = keyPairGenerator.generateKeyPair();
-        final PrivateKey privateKey = rsaKeys.getPrivate();
-        final PublicKey publicKey = rsaKeys.getPublic();
-        
-        rsaClient = Cipher.getInstance(RSA);
-        rsaServer = Cipher.getInstance(RSA);
-        rsaClient.init(Cipher.ENCRYPT_MODE, publicKey);
-        rsaServer.init(Cipher.DECRYPT_MODE, privateKey);
-        
-        byte[] encrypted = rsaClient.doFinal(sharedSecret),
-                decrypted = rsaServer.doFinal(encrypted);
-        assert(Arrays.equals(sharedSecret, decrypted));
-        final IvParameterSpec spec = new IvParameterSpec(decrypted);
-        
-        serverEncryption = Cipher.getInstance(AES);
-        serverDecryption = Cipher.getInstance(AES);
-        clientEncryption = Cipher.getInstance(AES);
-        clientDecryption = Cipher.getInstance(AES);
-        serverEncryption.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(decrypted, "AES"), spec);
-        serverDecryption.init(Cipher.DECRYPT_MODE, new SecretKeySpec(decrypted, "AES"), spec);
-        clientEncryption.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(decrypted, "AES"), spec);
-        clientDecryption.init(Cipher.DECRYPT_MODE, new SecretKeySpec(decrypted, "AES"), spec);
-        
-        server.onConnect(client -> {
-            client.setEncryption(serverEncryption);
-            client.setDecryption(serverDecryption);
-            client.readBytes(128, it -> System.out.println(Arrays.toString(it)));
-            client.readString(System.out::println);
-            
-        });
-        
-        server.bind("localhost", 9000);
         random.nextBytes(encryptBytes);
-        System.out.println(Arrays.toString(encryptBytes));
+    }
+    
+    @Test
+    void testPaddingAES() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, InterruptedException {
+        final Server server = new Server();
+        server.bind("localhost", 9000);
+        final String AES = "AES/CBC/PKCS5Padding";
+        final Cipher
+                a = Cipher.getInstance(AES),
+                b = Cipher.getInstance(AES),
+                c = Cipher.getInstance(AES),
+                d = Cipher.getInstance(AES);
+        initCiphers(a, b, c, d);
+        startTest(server, 9000, a, b, c, d, Cipher::doFinal, Cipher::doFinal);
+    }
+    
+    @Test
+    void testNoPaddingAES() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, InterruptedException {
+        final Server server = new Server();
+        server.bind("localhost", 9001);
+        final String AES = "AES/CFB8/NoPadding";
+        final Cipher
+                a = Cipher.getInstance(AES),
+                b = Cipher.getInstance(AES),
+                c = Cipher.getInstance(AES),
+                d = Cipher.getInstance(AES);
+        initCiphers(a, b, c, d);
+        startTest(server, 9001, a, b, c, d, Cipher::update, Cipher::update);
+    }
+    
+    private void initCiphers(
+            Cipher serverEncryption,
+            Cipher serverDecryption,
+            Cipher clientEncryption,
+            Cipher clientDecryption
+    ) throws InvalidAlgorithmParameterException, InvalidKeyException {
+        final IvParameterSpec ivSpec = new IvParameterSpec(sharedSecret);
+        final SecretKeySpec keySpec = new SecretKeySpec(sharedSecret, "AES");
+        serverEncryption.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+        serverDecryption.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+        clientEncryption.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+        clientDecryption.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+    }
+    
+    private void startTest(
+            Server server,
+            int port,
+            Cipher serverEncryption,
+            Cipher serverDecryption,
+            Cipher clientEncryption,
+            Cipher clientDecryption,
+            CryptoFunction encryption,
+            CryptoFunction decryption
+    ) throws InterruptedException {
+        server.onConnect(client -> {
+            client.setEncryption(serverEncryption, encryption);
+            client.setDecryption(serverDecryption, decryption);
+            client.readBytes(128, it -> {
+                assert(Arrays.equals(it, encryptBytes));
+            });
+            client.readString(it -> {
+                assert ("Hello World!".equals(it));
+            });
         
+        });
+    
         final Client client = new Client();
         client.onConnect(() -> {
-            client.setEncryption(clientEncryption);
-            client.setDecryption(clientDecryption);
+            client.setEncryption(clientEncryption, encryption);
+            client.setDecryption(clientDecryption, decryption);
             Packet.builder().putBytes(encryptBytes).putString("Hello World!").writeAndFlush(client);
         });
-        client.connect("localhost", 9000);
+        client.connect("localhost", port);
         Thread.sleep(5000);
     }
     
